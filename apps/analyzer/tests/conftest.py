1e2c05901a646a9165d52329403f9e58d7608ed9
anshux1 <anshukrs@icloud.com>
2026-09-02 05:11:40 +0530

    test(analyzer): isolate tests from host environment

--- apps/analyzer/tests/conftest.py
+++ apps/analyzer/tests/conftest.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault("APP_ENV", "test")
+os.environ["APP_ENV"] = "test"
 os.environ.setdefault("DATABASE_URL", "postgresql://mailsentinel:mailsentinel@localhost:5432/mailsentinel")
 os.environ.setdefault("S3_ACCESS_KEY_ID", "mailsentinel")
 os.environ.setdefault("S3_SECRET_ACCESS_KEY", "mailsentinel-secret")
