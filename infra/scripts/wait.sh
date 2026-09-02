744d9d0efc1ad7be15ea94db698111f6261406e7
anshux1 <anshukrs@icloud.com>
2026-09-02 05:52:41 +0530

    fix(infra): wait only on long-running services

--- infra/scripts/wait.sh
+++ infra/scripts/wait.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 set -eu
 . "$(dirname "$0")/common.sh"
-$COMPOSE up -d --wait postgres redis minio minio-init analyzer worker
+$COMPOSE up -d --wait postgres redis minio analyzer worker
 $COMPOSE ps
